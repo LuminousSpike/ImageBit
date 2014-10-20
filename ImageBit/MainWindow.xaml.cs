292cbabaebb2419c3347114245d809783932e33c
Nathan Todd <nathan.j.todd@gmail.com>
2014-10-20 14:46:22 +1300

    Encapsulated WebP encoder code into its own class.

--- ImageBit/MainWindow.xaml.cs
+++ ImageBit/MainWindow.xaml.cs
@@ -5,9 +5,16 @@ using System.Diagnostics;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System;
+using ImageBit.Classes.Encoder;
 
 namespace ImageBit
 {
+    enum CurrentState
+    {
+        CurrentFile = 0,
+        TotalFiles = 1,
+        FileName = 2
+    }
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -18,17 +25,9 @@ namespace ImageBit
 
         BackgroundWorker Worker = new BackgroundWorker();
 
-        int RunningProcesses = 0;
-        int CompletedFiles = 0;
-
         string FolderInputPath;
         string FolderOutputPath;
         bool Converting = false;
-        
-        // Define constants for user state
-        private const int STATE_CURRENTFILE = 0;
-        private const int STATE_TOTALFILES = 1;
-        private const int STATE_FILENAME = 2;
 
         public MainWindow()
         {
@@ -67,8 +66,7 @@ namespace ImageBit
         private void Reset()
         {
             ListBoxLog.Items.Clear();
-            RunningProcesses = 0;
-            CompletedFiles = 0;
+            
         }
 
        /// <summary>
@@ -186,10 +184,10 @@ namespace ImageBit
             string[] userstate = e.UserState as string[];
 
             WriteToListBoxLog("Converting Image (" 
-                + userstate[STATE_CURRENTFILE] 
-                + " / " + userstate[STATE_TOTALFILES] 
+                + userstate[(int)CurrentState.CurrentFile] 
+                + " / " + userstate[(int)CurrentState.TotalFiles] 
                 + "): " 
-                + userstate[STATE_FILENAME]);
+                + userstate[(int)CurrentState.FileName]);
             
         }
 
@@ -211,9 +209,11 @@ namespace ImageBit
         void worker_DoWork(object sender, DoWorkEventArgs e)
         {
             BackgroundWorker worker = sender as BackgroundWorker;
-
             string[] files = e.Argument as string[];
-            ConvertToWebP(sender, files, e);
+
+            EncoderWebP encoder = new EncoderWebP(FolderOutputPath);
+
+            encoder.ConvertToWebP(sender, files, e);
         }
 
         /// <summary>
@@ -234,111 +234,6 @@ namespace ImageBit
             }
 
             return filesList.ToArray();
-        }
-
-        /// <summary>
-        /// Converts images to the webp format.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="files"></param>
-        /// <param name="e"></param>
-        private void ConvertToWebP(object sender, string[] files, DoWorkEventArgs e)
-        {
-            BackgroundWorker worker = sender as BackgroundWorker;
-
-            int index = 0;
-
-            while (index < files.Length)
-            {
-                if (worker.CancellationPending)
-                {
-                    e.Cancel = true;
-                    return;
-                }
-                else
-                {
-                    index = SetupConversionProcess(files, worker, index);
-                }
-            }
-        }
-
-        /// <summary>
-        /// Prepares the conversion process
-        /// </summary>
-        /// <param name="files">Files we will be converting</param>
-        /// <param name="worker">Conversion worker</param>
-        /// <param name="index">Current location in files to convert</param>
-        /// <returns></returns>
-        private int SetupConversionProcess(string[] files, BackgroundWorker worker, int index)
-        {
-            if (RunningProcesses < Environment.ProcessorCount)
-            {
-                string file = files[index];
-
-                string exePath = Path.GetFullPath(@"Encoders\cwebp.exe");
-                string output = FolderOutputPath + @"\" + Path.GetFileNameWithoutExtension(file) + ".webp";
-                string args = "-lossless " + '"' + file + '"' + " -o " + '"' + output + '"';
-
-                InitializeWebp(exePath, args);
-
-                string[] userState = getNewUserState(files.Length, index, file);
-
-                worker.ReportProgress(index + 1, userState);
-
-                // Increment the counter variables.
-                RunningProcesses++;
-                index++;
-            }
-            else
-            {
-                System.Threading.Thread.Sleep(100);
-            }
-            return index;
-        }
-
-        /// <summary>
-        /// Generates a new userstate based current file
-        /// </summary>
-        /// <param name="fileLength">Total number of files</param>
-        /// <param name="index">Current index</param>
-        /// <param name="file">Current file name</param>
-        /// <returns></returns>
-        private static string[] getNewUserState(int fileLength, int index, string file)
-        {
-            // This sends the information we want to display to the listbox.
-            string[] userState = new string[3];
-            userState[STATE_CURRENTFILE] =  (index + 1).ToString();
-            userState[STATE_TOTALFILES] = fileLength.ToString();
-            userState[STATE_FILENAME] = Path.GetFileName(file);
-            return userState;
-        }
-
-        /// <summary>
-        /// Sets up the Webp converter
-        /// </summary>
-        /// <param name="exePath">Location of the executable</param>
-        /// <param name="args">Arguments to pass to converter</param>
-        private void InitializeWebp(string exePath, string args)
-        {
-            // This is the code to start the cwebp.exe process with the wanted arguments.
-            Process converter = new Process();
-            converter.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-            converter.StartInfo.FileName = exePath;
-            converter.StartInfo.Arguments = args;
-            converter.EnableRaisingEvents = true;
-            converter.Exited += new System.EventHandler(converter_Exited);
-            converter.Start();
-        }
-
-        /// <summary>
-        /// Executed when a converter process exits
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void converter_Exited(object sender, System.EventArgs e)
-        {
-            RunningProcesses--;
-            CompletedFiles++;
         }
     }
 }
